b989c91b3e0418a0e93d79c88bad577bacc2e1e4
Balasubramaniam077 <19cs020@kpriet.ac.in>
2023-07-22 14:08:19 +0000

    upfate

--- reactapp/reactapp/src/App.js
+++ reactapp/reactapp/src/App.js
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
 import logo from './logo.svg';
-=======
->>>>>>> 892221a781e0e2f58602875bb91e9be4acf23f98
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -22,9 +18,6 @@ function App() {
           Learn React
         </a>
       </header>
-=======
-      Hari 
->>>>>>> 892221a781e0e2f58602875bb91e9be4acf23f98
     </div>
   );
 }
