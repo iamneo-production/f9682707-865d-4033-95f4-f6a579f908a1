c3d20147637d44a1c1a32f549e58ae4ac51b0a8b
Kishore876 <Kishoreds876@gmail.com>
2023-07-24 13:54:54 +0000

    ghjkl

--- reactapp/src/pages/login/App.js
+++ reactapp/src/pages/login/App.js
@@ -27,7 +27,7 @@ function LoginPage() {
       "password": pass
    }
 
-   axios.post("https://8080-cdcaeebeddfbdcceabcedefcacfefaabeaeaadbdbabf.project.examly.io/login",credentails).then(res=>{setrest(res.data)
+   axios.post("https://8080-cdcaeebeddfbdcceabcedefcacfefaabebceecbecadec.project.examly.io/login",credentails).then(res=>{setrest(res.data)
   
   }).catch(err=>console.log(err)); 
 
